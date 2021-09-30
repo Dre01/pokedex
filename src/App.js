60477c01a2e1499d0ed4488ff6cef1006be8e93e
dre <dredelwyn@gmail.com>
2021-09-30 16:22:56 +0200

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Pokecard from './Pokecard';
+import './App.css';
+import Pokedex from './Pokedex';
+import Pokegame from './Pokegame';
+
+function App() {
+  return (
+    <div className="App">
+        <Pokegame/>
+    </div>
+  );
+}
+
+export default App;
